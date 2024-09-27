63e621b654d9d088d2a13d478eba0684a79010a9
ycesrom <ycesrom0111@g.educaand.es>
2024-09-27 12:02:39 +0200

    Ejercicios Tema 1

---
+++ LC/Tema1/Ejercicios 1-5 Tema 1 Tipos de datos simples.py
@@ -0,0 +1,22 @@
+#Ejercicio 1
+print("Hola Mundo")
+
+#Ejercicio 2
+instituto="IES MURGIS"
+print(instituto)
+
+#Ejercicio 3
+nombre=str(input("Introduzca su nombre "))
+print("Hola ", nombre)
+
+#Ejercicio 4
+operacion=(3+2)/(2*5)
+final=operacion**2
+print(final)
+
+#Ejercicio 5
+
+horas=int(input("Introduzca el numero de horas trabajadas "))
+coste=float(input("Introduzca el coste por hora "))
+pago=horas*coste
+print("Su salario es de ",pago)
