aa24234fac174b2dd6ada676eec419de498232dc
ycesrom <ycesrom0111@g.educaand.es>
2024-10-19 15:55:21 +0200

    Actualizado

--- DI/Calculadora/src/main/java/com/example/calculadora/HelloApplication.java
+++ DI/Calculadora/src/main/java/com/example/calculadora/HelloApplication.java
@@ -9,23 +9,19 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400); // Ajusta el tamaño aquí
         scene.getStylesheets().add(getClass().getResource("estilos.css").toExternalForm());
         stage.setMinHeight(300);
         stage.setMinWidth(400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Calculadora"); // Cambia el título para reflejar la aplicación
         stage.setScene(scene);
         stage.show();
-        scene.getStylesheets().add(getClass().getResource("estilos.css").toExternalForm());
     }
 
     public static void main(String[] args) {
         launch();
     }
-
-
 }
