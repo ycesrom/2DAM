0e2ffca3b43b66e344562c2b1a628de4d264483e
Yerai <ycesrom0111@g.educaand.es>
2024-11-22 10:00:15 +0100

    Actualizado

--- AAD/Acceso a Datos WorkSpace 24-25/ConexionBD/src/mySql/MenuBD.java
+++ AAD/Acceso a Datos WorkSpace 24-25/ConexionBD/src/mySql/MenuBD.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet; 
-import java.sql.Statement;
 import java.util.Scanner; 
 
 	public class MenuBD 
@@ -22,14 +21,10 @@ import java.util.Scanner;
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/basedatos", "root",""); 
 			System.out.println("Conexión establecida"); 
 			 
-			Statement mistatement = con.createStatement(); 
 
-			String sql = "CREATE TABLE IF NOT EXISTS libros (" +
-                    "id INT AUTO_INCREMENT PRIMARY KEY," +
-                    "titulo VARCHAR(255) NOT NULL," +
-                    "autor VARCHAR(255) NOT NULL," +
-                    "disponibilidad BOOLEAN DEFAULT TRUE)";
-			mistatement.executeUpdate(sql);
+			String sql = "CREATE TABLE IF NOT EXISTS libros (id INT AUTO_INCREMENT PRIMARY KEY,titulo VARCHAR(255) NOT NULL,autor VARCHAR(255) NOT NULL,disponibilidad BOOLEAN DEFAULT TRUE,anioPublicacion INT NOT NULL)";
+		    PreparedStatement pstmt = con.prepareStatement(sql); 
+		    pstmt.executeUpdate(sql);
 			            // Cerrar recursos 
 			          
 			            con.close(); 
@@ -47,30 +42,34 @@ import java.util.Scanner;
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/basedatos", "root",""); 
 			System.out.println("Conexión establecida"); 
 			 
-			Scanner entrada=new Scanner(System.in);
-			String titulo,autor;
-			boolean disponibilidad;
-			System.out.println("Introduzca el titulo ");
-			titulo=entrada.nextLine();
-			System.out.println("Introduzca el autor ");
-			autor=entrada.nextLine();
-			System.out.println("Introduzca la disponibilidad ");
-			disponibilidad=entrada.nextBoolean();
+			try (Scanner entrada = new Scanner(System.in)) {
+				String titulo,autor;
+				boolean disponibilidad;
+				int anioPublicacion;
+				System.out.println("Introduzca el titulo ");
+				titulo=entrada.nextLine();
+				System.out.println("Introduzca el autor ");
+				autor=entrada.nextLine();
+				System.out.println("Introduzca la disponibilidad ");
+				disponibilidad=entrada.nextBoolean();
+				System.out.println("Introduzca el año de publicacion ");
+				anioPublicacion=entrada.nextInt();
 
 
-			String sql = "INSERT INTO libros (titulo, autor, disponibilidad) VALUES (?, ?, ?)";	
+				String sql = "INSERT INTO libros (titulo, autor, disponibilidad,anioPublicacion) VALUES (?, ?, ?,?)";	
 
-			
-			 PreparedStatement pstmt = con.prepareStatement(sql);
-	            
-	            // Establecer los parámetros del PreparedStatement
-	            pstmt.setString(1, titulo);
-	            pstmt.setString(2, autor);
-	            pstmt.setBoolean(3, disponibilidad);
-	            pstmt.executeUpdate();
-			            // Cerrar recursos 
-			          
-			            con.close(); 
+				
+				 PreparedStatement pstmt = con.prepareStatement(sql);
+				    
+				    // Establecer los parámetros del PreparedStatement
+				    pstmt.setString(1, titulo);
+				    pstmt.setString(2, autor);
+				    pstmt.setBoolean(3, disponibilidad);
+				    pstmt.setInt(4, anioPublicacion);
+				    pstmt.executeUpdate();
+				            // Cerrar recursos 
+			}
+			con.close(); 
 			 
 			        } catch (Exception e) { 
 			            System.out.println("Error: " + e); 
@@ -88,19 +87,20 @@ import java.util.Scanner;
 				Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/basedatos", "root","");
 				System.out.println("Conexion establecida");
 				
-				Scanner entrada=new Scanner(System.in);
-				boolean disponibilidad;
-				int id;
-				System.out.println("Introduzca la id del libro: ");
-				id=entrada.nextInt();
-				System.out.println("Introduzca la disponibilidad del libro: ");
-				disponibilidad=entrada.nextBoolean();
-				
-				String sql="UPDATE libros SET disponibilidad=? WHERE id=?";
-				PreparedStatement pstmt = con.prepareStatement(sql);
-				pstmt.setBoolean(1, disponibilidad);
-				pstmt.setInt(2, id);
-				pstmt.executeUpdate();
+				try (Scanner entrada = new Scanner(System.in)) {
+					boolean disponibilidad;
+					int id;
+					System.out.println("Introduzca la id del libro: ");
+					id=entrada.nextInt();
+					System.out.println("Introduzca la disponibilidad del libro: ");
+					disponibilidad=entrada.nextBoolean();
+					
+					String sql="UPDATE libros SET disponibilidad=? WHERE id=?";
+					PreparedStatement pstmt = con.prepareStatement(sql);
+					pstmt.setBoolean(1, disponibilidad);
+					pstmt.setInt(2, id);
+					pstmt.executeUpdate();
+				}
 			}catch(Exception e) 
 			{
 				System.out.println("Error: "+e);
@@ -111,7 +111,7 @@ import java.util.Scanner;
 		
 		public static void eliminarLibro() 
 		{
-String driver="com.mysql.cj.jdbc.Driver";
+			String driver="com.mysql.cj.jdbc.Driver";
 			
 			try 
 			{
@@ -119,15 +119,16 @@ String driver="com.mysql.cj.jdbc.Driver";
 				Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/basedatos", "root","");
 				System.out.println("Conexion establecida");
 				
-				Scanner entrada=new Scanner(System.in);
-				int id;
-				System.out.println("Introduzca la id del libro: ");
-				id=entrada.nextInt();
-				
-				String sql="DELETE FROM libros WHERE id=?";
-				PreparedStatement pstmt = con.prepareStatement(sql);
-				pstmt.setInt(1,id);
-				pstmt.executeUpdate();
+				try (Scanner entrada = new Scanner(System.in)) {
+					int id;
+					System.out.println("Introduzca la id del libro: ");
+					id=entrada.nextInt();
+					
+					String sql="DELETE FROM libros WHERE id=?";
+					PreparedStatement pstmt = con.prepareStatement(sql);
+					pstmt.setInt(1,id);
+					pstmt.executeUpdate();
+				}
 			}catch(Exception e) 
 			{
 				System.out.println("Error: "+e);
@@ -140,25 +141,31 @@ String driver="com.mysql.cj.jdbc.Driver";
 	   {
 		   String driver="com.mysql.cj.jdbc.Driver";
 			
-			try 
+			try (Scanner entrada = new Scanner(System.in)) 
 			{
 				Class.forName(driver);
 				Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/basedatos", "root","");
 				System.out.println("Conexion establecida");
-				String sql="SELECT * FROM libros";
-		
-				Statement mistatement = con.createStatement();
-	            ResultSet resultadoLibros = mistatement.executeQuery(sql);
+				boolean disponibilidad=false;
+				System.out.println("Introduzca la disponibilidad");
+				disponibilidad=entrada.nextBoolean();
+				
+				String sql="SELECT * FROM libros WHERE disponibilidad=?";
+				
+				PreparedStatement mistatement = con.prepareStatement(sql);
+				mistatement.setBoolean(1, disponibilidad);
+	            ResultSet resultadoLibros = mistatement.executeQuery();
 	            while (resultadoLibros.next()) {
 	                int id = resultadoLibros.getInt("id");
 	                String titulo = resultadoLibros.getString("titulo");
 	                String autor = resultadoLibros.getString("autor");
-	                boolean disponibilidad = resultadoLibros.getBoolean("disponibilidad");
+	                int anioPublicacion=resultadoLibros.getInt("anioPublicacion");
 
 	                System.out.println("ID: " + id);
 	                System.out.println("Título: " + titulo);
 	                System.out.println("Autor: " + autor);
 	                System.out.println("Disponibilidad: " + disponibilidad);
+	                System.out.println("Año Publicacion: "+anioPublicacion);
 	              
 	            }
 	   }catch(Exception e ) 
@@ -170,28 +177,30 @@ String driver="com.mysql.cj.jdbc.Driver";
 	{
 		int opcion=0;
 		do {
-			Scanner entrada=new Scanner(System.in);
-		System.out.println("1.Crear la tabla libros si no existe");
-		System.out.println("2.Insertar un nuevo libro");
-		System.out.println("3.Modificar la disponiblidad de un libro");
-		System.out.println("4.Consultar todos los linros, por bien disponibles o bien prestados");
-		System.out.println("5.Eliminar un libro por su ID");
-		System.out.println("6.Salir del programa");
-		opcion=entrada.nextInt();
-		switch(opcion) 
-		{	
-			case 1->{crearTabla();}
-			case 2->{insertarLibro();}
-			case 3->{modificarLibro();}
-			case 4->{consultarLibros();}
-			case 5->{eliminarLibro();}
-			default->
-			{
-				System.out.println("Has salido del programa");
+		try (Scanner entrada = new Scanner(System.in)) {
+			System.out.println("1.Crear la tabla libros si no existe");
+			System.out.println("2.Insertar un nuevo libro");
+			System.out.println("3.Modificar la disponiblidad de un libro");
+			System.out.println("4.Consultar todos los linros, por bien disponibles o bien prestados");
+			System.out.println("5.Eliminar un libro por su ID");
+			System.out.println("6.Salir del programa");
+			opcion=entrada.nextInt();
+			switch(opcion) 
+			{	
+				case 1->{crearTabla();}
+				case 2->{insertarLibro();}
+				case 3->{modificarLibro();}
+				case 4->{consultarLibros();}
+				case 5->{eliminarLibro();}
+				default->
+				{
+					System.out.println("Has salido del programa");
+				}
+			
 			}
-		
 		}
 		}while(opcion!=6);
+		
 		 
 	}
 } 
