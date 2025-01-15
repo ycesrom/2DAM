f177eb9d8ebc8fb871e02df29c1cb87ced549643
Yerai <ycesrom0111@g.educaand.es>
2025-01-15 08:48:31 +0000

    Actualizado

--- AAD/CRUD_Yerai_Cespedes_Romera/src/main/java/com/mycompany/crud_yerai_cespedes_romera/CRUD_Yerai_Cespedes_Romera.java
+++ AAD/CRUD_Yerai_Cespedes_Romera/src/main/java/com/mycompany/crud_yerai_cespedes_romera/CRUD_Yerai_Cespedes_Romera.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.crud_yerai_cespedes_romera;
 
+import java.util.Scanner;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -14,10 +15,40 @@ import javax.persistence.Persistence;
  */
 public class CRUD_Yerai_Cespedes_Romera {
 
-    public static void main(String[] args) {
-EntityManagerFactory emf= Persistence.createEntityManagerFactory("com.mycompany_CRUD_Yerai_Cespedes_Romera_jar_1.0-SNAPSHOTPU");
-EntityManager em=emf.createEntityManager();
-        System.out.println("Hello World!");
+    public static void main(String[] args) 
+    {
+    EntityManagerFactory emf= Persistence.createEntityManagerFactory("com.mycompany_CRUD_Yerai_Cespedes_Romera_jar_1.0-SNAPSHOTPU");
+    EntityManager em=emf.createEntityManager();
+        
+    }
+    
+    public static void menu()
+    {
+        int opcion=0;
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Introduzca una opcion");
+        opcion=entrada.nextInt();
+        switch(opcion)
+        {
+           
+            
+        }
+    }
+    
+    public static void añadirProducto()
+    {
+    }
+    
+    public static void eliminarProducto()
+    {
+    }
+    
+    public static void actualizarLibro()
+    {
+    }
+    
+    public static void mostrarProductos()
+    {
     }
     
     
