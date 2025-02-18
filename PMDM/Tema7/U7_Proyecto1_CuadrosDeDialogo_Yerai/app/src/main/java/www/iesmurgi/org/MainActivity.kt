24db6d37ce6ab2c6a5ec5fa0d6c5f986d502fadd
ycesrom <ycesrom0111@g.educaand.es>
2025-02-18 21:20:57 +0100

    Actualizado

--- PMDM/Tema7/U7_Proyecto1_CuadrosDeDialogo_Yerai/app/src/main/java/www/iesmurgi/org/MainActivity.kt
+++ PMDM/Tema7/U7_Proyecto1_CuadrosDeDialogo_Yerai/app/src/main/java/www/iesmurgi/org/MainActivity.kt
@@ -1,6 +1,5 @@
 package www.iesmurgi.org
 
-import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
@@ -22,7 +20,6 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -39,7 +36,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import www.iesmurgi.org.ui.theme.U7_Proyecto1_CuadrosDeDialogo_YeraiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
